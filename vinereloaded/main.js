440f7a07a35d426c5cd8a44a45aa4987ee6896a1
isaacgravenortechnologies <83920636+isaacgravenortechnologies@users.noreply.github.com>
2022-05-25 18:23:19 +0100

    Vine Reloaded Alpha 1
    5 popular old vines have been added. Use "issues" tab to request more.
    Search works and video playback.

--- vinereloaded/main.js
+++ vinereloaded/main.js
@@ -2,3 +2,17 @@ function doSearch() {
     query = document.getElementById("search").value
     window.location.replace("search.html?query="+query);
 }
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+}
+wait(500)
+document.getElementById("search").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("searchbutton").click();
+    }
+});
